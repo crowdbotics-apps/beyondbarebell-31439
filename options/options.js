7f735821c3ed6bb95594609e83fcc895eb2a2c4f
Crowdbotics <team@crowdbotics.com>
2021-10-15 09:44:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "beyondbarebell_31439",
+  url: "https://beyondbarebell_31439.botics.co",
+  api: "https://beyondbarebell_31439.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
